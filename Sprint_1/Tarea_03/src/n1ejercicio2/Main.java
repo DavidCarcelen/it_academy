bdee7e207d52f6936f10a77ae628b67c33b9a953
davidcarcelen <davidcarcelengenova@gmail.com>
2024-03-18 23:02:27 +0100

    file reader

--- Sprint_1/Tarea_03/src/n1ejercicio2/Main.java
+++ Sprint_1/Tarea_03/src/n1ejercicio2/Main.java
@@ -9,19 +9,30 @@ import java.util.ListIterator;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer>lista1 = new ArrayList<>();
+        List<Integer> lista1 = new ArrayList<>();
 
         lista1.add(0,60);
         lista1.add(1,61);
         lista1.add(2,62);
-        lista1.add(2,63);
+        lista1.add(3,63);
 
-        List<Integer>lista2 = new ArrayList<>();
+        List<Integer> lista2 = new ArrayList<>();
 
         ListIterator<Integer> intIterator = lista1.listIterator();
 
+        System.out.println("Elementos en lista uno:");
+
         while(intIterator.hasNext()) {
             System.out.println(intIterator.next());
+        }
+
+        while(intIterator.hasPrevious()) {
+            lista2.add(intIterator.previous());
+        }
+        System.out.println("Elementos en lista dos en orden inverso:");
+
+        for (int i: lista2){
+            System.out.println(i);
         }
     }
 }
---
+++ Sprint_1/Tarea_03/src/n1ejercicio3/Main.java
@@ -0,0 +1,61 @@
+package n1ejercicio3;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("/Users/davidcarcelen/Desktop/countries.txt");
+        String text = "";
+        BufferedReader br = null;
+        HashMap <String, String> countryCapital = new HashMap<String, String>();
+        String name = "";
+        int num = 0;
+        int puntuacion = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        try {
+            br = new BufferedReader(new FileReader(file));
+            while ((text = br.readLine())!= null){
+                String[] parts = text.split(" ");
+                countryCapital.put(parts[0], parts[1]);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }  catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
+        Random random = new Random();
+        List<String> keys = new ArrayList<String>(countryCapital.keySet());
+
+        System.out.println("Introduce nombre de usuario:");
+        name = input.next();
+        System.out.println("Bienvenid@ " + name + "!");
+        while (num != 10){
+            String randomCountry = keys.get(random.nextInt(countryCapital.size()));
+            System.out.println("Cuál es la capital de: " + randomCountry);
+            String value = countryCapital.get(randomCountry);
+            String respuesta = input.next();
+            if (respuesta.equalsIgnoreCase(value)){
+                puntuacion ++;
+            }
+            num ++;
+        }
+        System.out.println("Has conseguido " + puntuacion + " aciertos.");
+
+      try {
+          File archivo = new File("/Users/davidcarcelen/Desktop/classificacio.txt");
+          FileWriter escribir = new FileWriter(archivo, true);
+          escribir.write(name);
+          escribir.write(puntuacion);
+      } catch (Exception e){
+          throw new RuntimeException(e);
+        }
+
+
+
+    }
+
+}
